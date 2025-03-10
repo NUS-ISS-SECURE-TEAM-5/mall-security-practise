b81ce35934fe848a72395bcada4213736dfe8751
chenhui <1037823529@qq.com>
2025-03-10 20:55:09 +0800

    Oauth2.0

--- mall-mbg/src/main/java/com/macro/mall/model/UmsMemberExample.java
+++ mall-mbg/src/main/java/com/macro/mall/model/UmsMemberExample.java
@@ -267,11 +267,6 @@ public class UmsMemberExample {
             return (Criteria) this;
         }
 
-        public Criteria andSubEqualTo(String value) {
-            addCriterion("sub =", value, "sub");
-            return (Criteria) this;
-        }
-
         public Criteria andUsernameNotEqualTo(String value) {
             addCriterion("username <>", value, "username");
             return (Criteria) this;
